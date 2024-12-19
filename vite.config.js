f0fd388993316c6b64304924df86ba5b4317e9cb
Udhaya0519 <udhaya0519@gmail.com>
2024-12-19 18:04:54 +0530

    changed base URL

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: '/Tamil-Day-Calender/'
   //   build: {
   //     rollupOptions: {
   //       input: {
